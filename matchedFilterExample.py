5f121e16c6d5d3f460c079b7dd2db679482f9e3a
NouraDK <68608054+PhObOs20@users.noreply.github.com>
2024-05-22 16:05:55 +0200

    Update matchedFilterExample.py
    comments

--- matchedFilterExample.py
+++ matchedFilterExample.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal as sp
 
-sig = [1, 1, 0, 1, 1, 0, 0, 1]
+sig = [1, 1, 0, 1, 1, 0, 0, 1] #test code
 print(sig)
 fig=plt.figure(figsize=(10,8))
 plt.plot(sig, label='signal')
@@ -15,7 +15,7 @@ plt.title('An Example Signal')
 plt.grid()
 
 
-sig_inzeros=np.hstack([np.zeros(4),sig,np.zeros(5)]) # 4+7+5 = 16
+sig_inzeros=np.hstack([np.zeros(4),sig,np.zeros(5)]) #test code in lager signal
 plt.figure(figsize=(10,8))
 plt.plot(sig_inzeros)
 plt.xlabel('Sample')
@@ -24,7 +24,7 @@ plt.title('The Signal at Some Point in Time')
 plt.grid()
 
 
-signoise = np.random.rand(len(sig_inzeros))-0.5+sig_inzeros
+signoise = np.random.rand(len(sig_inzeros))-0.5+sig_inzeros # add noise
 plt.figure(figsize=(10,8))
 plt.plot(signoise)
 plt.xlabel('Sample')
@@ -34,9 +34,9 @@ plt.grid()
 plt.show()
 
 
-h = sig[::-1] # fliplr
+#h = sig[::-1] # fliplr marched filter
 #signoisemf = sp.lfilter(h, 1, signoise)
-output = sp.correlate(signoise, sig)
+output = sp.correlate(signoise, sig) #convolving with matched filter is the same as corralating with ref signal 
 plt.figure(figsize=(10,8))
 plt.plot(output)
 plt.xlabel('Sample')
