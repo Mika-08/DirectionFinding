47f662d4de5f399fe9ad690b0e4d7559f2e2e462
NouraDK <68608054+PhObOs20@users.noreply.github.com>
2024-05-22 16:03:16 +0200

    Create matchedFilterExample.py

---
+++ matchedFilterExample.py
@@ -0,0 +1,55 @@
+# Plotting & Display
+import matplotlib.pyplot as plt
+
+# Configurations
+import numpy as np
+import scipy.signal as sp
+
+sig = [1, 1, 0, 1, 1, 0, 0, 1]
+print(sig)
+fig=plt.figure(figsize=(10,8))
+plt.plot(sig, label='signal')
+plt.xlabel('Sample')
+plt.ylabel('Value')
+plt.title('An Example Signal')
+plt.grid()
+
+
+sig_inzeros=np.hstack([np.zeros(4),sig,np.zeros(5)]) # 4+7+5 = 16
+plt.figure(figsize=(10,8))
+plt.plot(sig_inzeros)
+plt.xlabel('Sample')
+plt.ylabel('Value')
+plt.title('The Signal at Some Point in Time')
+plt.grid()
+
+
+signoise = np.random.rand(len(sig_inzeros))-0.5+sig_inzeros
+plt.figure(figsize=(10,8))
+plt.plot(signoise)
+plt.xlabel('Sample')
+plt.ylabel('Value')
+plt.title('The Example Signal in Noise')
+plt.grid()
+plt.show()
+
+
+h = sig[::-1] # fliplr
+#signoisemf = sp.lfilter(h, 1, signoise)
+output = sp.correlate(signoise, sig)
+plt.figure(figsize=(10,8))
+plt.plot(output)
+plt.xlabel('Sample')
+plt.ylabel('Value')
+plt.title('The Example Signal in Noise after Macthed Filtering')
+plt.grid()
+plt.show()
+
+
+
+
+
+
+
+
+
