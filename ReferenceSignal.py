2483969c4bdf6e5a859e1beb6fc1e1853a98a82e
yuekroeze <yue.kroeze@quicknet.nl>
2024-05-31 15:12:33 +0200

    Added ReferenceSignal.py to and added cross correlation function to SignalProcessing.py

---
+++ ReferenceSignal.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+class ReferenceSignal:
+    def __init__(self, sample_rate):
+        self.signal = None
+        self.sample_rate = sample_rate
+        self.gold_code = [1, 1, 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0]
+
+    def create_reference_signal(self):
+        """
+        Function that creates a reference signal form the code
+        :return: Nothing
+        """
+        on_duration = int(0.03 * self.sample_rate)
+        off_duration_short = int(0.03 * self.sample_rate)
+        off_duration_long = int(0.91 * self.sample_rate)
+
+        reference_signal = np.concatenate([
+            np.ones(on_duration),
+            np.zeros(off_duration_short),
+            np.ones(on_duration),
+            np.zeros(off_duration_long)
+        ])
+
+        # Repeat the pattern to ensure it's long enough
+        repeat_count = int(np.ceil(self.sample_rate / len(reference_signal)))
+        reference_signal = np.tile(reference_signal, repeat_count)
+
+        self.signal = reference_signal
